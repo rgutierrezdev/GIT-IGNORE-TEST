651296d29edc2a74d693647334dc012ddf47f0a9
Rodrigo Ezequiel Gutierrez <rgutierrezdev@gmail.com>
2022-07-26 13:17:14 -0300

    Initial Commit

---
+++ HERMETYC-SECURE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HERMETYC_SECURE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
